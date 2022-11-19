fd668b4c6127fed1dcc3ea2b327e9cca08f6864f
Lucaslllll <lucascosta12367@gmail.com>
2022-11-18 21:07:26 -0300

    first version of screen diary

--- frontend/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/dists/yourdiary/obj/local/armeabi-v7a/objs-debug/webp/src/utils/quant_levels_utils.o.d
+++
@@ -1,8 +0,0 @@
-/home/Lucas/Documentos/YourDiary/frontend/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/armeabi-v7a/objs-debug/webp/src/utils/quant_levels_utils.o: \
-  /home/Lucas/Documentos/YourDiary/frontend/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp-1.0.0/src/utils/quant_levels_utils.c \
-  /home/Lucas/Documentos/YourDiary/frontend/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp-1.0.0/src/utils/quant_levels_utils.h \
-  /home/Lucas/Documentos/YourDiary/frontend/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp-1.0.0/src/webp/types.h
-
-/home/Lucas/Documentos/YourDiary/frontend/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp-1.0.0/src/utils/quant_levels_utils.h:
-
-/home/Lucas/Documentos/YourDiary/frontend/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp-1.0.0/src/webp/types.h:
